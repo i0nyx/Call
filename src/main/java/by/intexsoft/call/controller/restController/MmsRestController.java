c388a81bfc40946233f10a5ab52d29973bdb48e0
sergey.buzuk <sergey.buzuk@intexsoft.by>
2018-09-19 17:00:02 +0300

    add mms rest controller

--- src/main/java/by/intexsoft/call/controller/restController/MmsRestController.java
+++ src/main/java/by/intexsoft/call/controller/restController/MmsRestController.java
@@ -1,4 +1,34 @@
 package by.intexsoft.call.controller.restController;
 
+import by.intexsoft.call.pojo.Mms;
+import by.intexsoft.call.service.MmsService;
+import by.intexsoft.call.service.SaveToFileService;
+import by.intexsoft.call.util.DateConverter;
+import lombok.AllArgsConstructor;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/rest/")
+@AllArgsConstructor
 public class MmsRestController {
+    public final MmsService mmsService;
+    public final SaveToFileService fileService;
+
+    @PostMapping(value = "mms")
+    public void mmsInfo(@RequestBody String data){
+        JSONObject json = new JSONObject(data);
+        String type = json.getString("type");
+        int dataStart = DateConverter.stringToDate(json.getString("start"));
+        int dataEnd = DateConverter.stringToDate(json.getString("end"));
+        List<Mms> calls = mmsService.getMmsPeriodTime(type, dataStart, dataEnd);
+        Optional.ofNullable(calls).orElseThrow(NullPointerException::new);
+        fileService.saveToFile(calls, type);
+    }
 }
--- src/main/java/by/intexsoft/call/service/MmsService.java
+++ src/main/java/by/intexsoft/call/service/MmsService.java
@@ -1,4 +1,19 @@
 package by.intexsoft.call.service;
 
+import by.intexsoft.call.pojo.Mms;
+
+import java.util.List;
+
+/**
+ *
+ */
 public interface MmsService {
+
+    /**
+     * @param type
+     * @param start
+     * @param end
+     * @return
+     */
+    List<Mms> getMmsPeriodTime(String type, int start, int end);
 }
