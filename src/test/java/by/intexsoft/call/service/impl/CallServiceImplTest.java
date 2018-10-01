4d0328944f4954902e6ea7a63863b3c742d97466
Sergey.Buzuk <0nyx@tut.by>
2018-10-01 22:15:14 +0300

    edit test

--- src/test/java/by/intexsoft/call/service/impl/CallServiceImplTest.java
+++ src/test/java/by/intexsoft/call/service/impl/CallServiceImplTest.java
@@ -3,13 +3,18 @@ package by.intexsoft.call.service.impl;
 import by.intexsoft.call.repositories.CallRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.ArrayList;
 import java.util.Date;
 
-import static org.mockito.Matchers.anyList;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({CallServiceImpl.class})
 public class CallServiceImplTest {
     private CallServiceImpl callService;
     private CallRepository callRepository;
@@ -23,8 +28,7 @@ public class CallServiceImplTest {
     @Test
     public void loadObjectByTime() {
         callService.loadObjectByTime(new Date(), new Date());
-        when(callRepository.findAllByPeriod(new Date(), new Date())).thenReturn(anyList());
-
+        when(callRepository.findAllByPeriod(new Date(), new Date())).thenReturn(new ArrayList<>());
     }
 
     @Test
---
+++ src/test/java/by/intexsoft/call/service/impl/LoadServiceImplTest.java
@@ -0,0 +1,30 @@
+package by.intexsoft.call.service.impl;
+
+import by.intexsoft.call.service.ConvertService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({LoadServiceImpl.class})
+public class LoadServiceImplTest {
+    private List<ConvertService> convertServices;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void load() {
+    }
+
+    @Test
+    public void getType() {
+    }
+}
