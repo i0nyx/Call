e678689fe9641d536341c17c788cb8c094d55dd9
sergey.buzuk <sergey.buzuk@intexsoft.by>
2018-10-03 10:12:51 +0300

    add test for LoaderServiceImpl class

--- src/test/java/by/intexsoft/call/service/impl/LoadServiceImplTest.java
+++ src/test/java/by/intexsoft/call/service/impl/LoadServiceImplTest.java
@@ -1,6 +1,8 @@
 package by.intexsoft.call.service.impl;
 
 import by.intexsoft.call.pojo.RequestObject;
+import by.intexsoft.call.pojo.type.Type;
+import by.intexsoft.call.repositories.CallRepository;
 import by.intexsoft.call.service.ConvertService;
 import by.intexsoft.call.service.LoaderService;
 import org.junit.Before;
@@ -8,37 +10,55 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static by.intexsoft.call.pojo.type.Type.CALL;
+import static org.junit.Assert.assertEquals;
 import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
 
+/**
+ * This test checks the class performance {@link LoadServiceImpl}
+ */
 @RunWith(PowerMockRunner.class)
 public class LoadServiceImplTest {
-    private LoaderService loadService;
     private List<ConvertService> converters;
+    private ConvertService callConvertService;
 
-
+    /**
+     * Setting initial parameters
+     */
     @Before
     public void setUp() {
+        CallRepository repository = mock(CallRepository.class);
         converters = new ArrayList<>();
-        CallConvertServiceImpl callConvertService = mock(CallConvertServiceImpl.class);
+        callConvertService = new CallConvertServiceImpl(repository);
         converters.add(callConvertService);
     }
 
+    /**
+     * Check of return types when loading data from the DB
+     */
     @Test
     public void load() {
-        loadService = new LoadServiceImpl(converters);
-        RequestObject requestObject = new RequestObject();
-        loadService.load(requestObject);
-        verify(loadService, times(1)).getType(any());
+        LoaderService loadService = new LoadServiceImpl(converters);
+        loadService.load(buildRequestObject());
+        assertEquals(loadService.getType("call"), CALL);
+        Map<Type, ConvertService> map = new HashMap<>();
+        map.put(CALL, callConvertService);
+        assertEquals(map.get(CALL), callConvertService);
+        when(callConvertService.loadObjectByTime(buildRequestObject())).thenReturn(new ArrayList());
     }
 
+    /**
+     * Return type check
+     */
     @Test
     public void getType() {
+        assertEquals(Type.valueOf("call".toUpperCase()), CALL);
+    }
 
+    private RequestObject buildRequestObject() {
+        return RequestObject.builder().type("call").startDate(new Date()).endDate(new Date()).build();
     }
 }
