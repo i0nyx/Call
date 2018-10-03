5dd9bd46ed3156863fce05f0ec13257e6f3dca47
sergey.buzuk <sergey.buzuk@intexsoft.by>
2018-10-03 11:23:48 +0300

    refactor DateConverterTest

--- src/test/java/by/intexsoft/call/util/DateConverterTest.java
+++ src/test/java/by/intexsoft/call/util/DateConverterTest.java
@@ -1,8 +1,8 @@
 package by.intexsoft.call.util;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.slf4j.Logger;
@@ -12,36 +12,35 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.*;
 
-/*
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({DateConverter.class, LoggerFactory.class})
 public class DateConverterTest {
+    @Mock
     private SimpleDateFormat dateFormat;
-
-    @Before
-    public void setUp() {
-        dateFormat = mock(SimpleDateFormat.class);
-    }
 
     @Test
     public void stringToDate() throws ParseException {
         String dateStr = "2018-09-18 04:28:25";
         Date date = new Date();
         DateConverter.stringToDate(dateStr);
-        when(dateFormat.parse(anyString())).thenReturn(date);
+        when(dateFormat.parse(dateStr)).thenReturn(date);
         verify(dateFormat);
     }
 
-    @Test
-    public void failConvert() {
+    @Test(expected = ParseException.class)
+    public void failConvert() throws ParseException {
+        String dateStr = "2018-09-18 04:28:25";
         mockStatic(LoggerFactory.class);
         Logger log = mock(Logger.class);
+        doThrow(new ParseException("error", 1))
+                .when(dateFormat)
+                .parse(any());
+        dateFormat.parse(dateStr);
         when(LoggerFactory.getLogger(DateConverter.class)).thenReturn(log);
-        DateConverter.stringToDate(anyString());
-        verify(log);
+        verify(log).error("error");
     }
-}*/
+}
